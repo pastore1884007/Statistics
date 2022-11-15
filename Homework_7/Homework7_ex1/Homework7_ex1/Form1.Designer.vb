66ce51001ade1dd318e5bd540453c882945fc0b6
pastore1884007 <109091630+pastore1884007@users.noreply.github.com>
2022-11-15 15:30:09 +0100

    Add files via upload

---
+++ Homework_7/Homework7_ex1/Homework7_ex1/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(723, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Absolute Frequency"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(839, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 40)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Relative Frequency"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(955, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 40)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Normalized Frequency"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 24)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Minimum = 1
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar1.TabIndex = 3
+        Me.TrackBar1.Value = 1
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(149, 24)
+        Me.TrackBar2.Maximum = 30
+        Me.TrackBar2.Minimum = 1
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar2.TabIndex = 4
+        Me.TrackBar2.Value = 1
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(339, 24)
+        Me.TrackBar3.Maximum = 100
+        Me.TrackBar3.Minimum = 1
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar3.TabIndex = 5
+        Me.TrackBar3.Value = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 15)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Number of Trials:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(149, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Number of Trajectories:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(339, 6)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Value of λ:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 75)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(477, 701)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(495, 75)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(404, 701)
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(905, 75)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(486, 701)
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1403, 788)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ Homework_7/Homework7_ex1/Homework7_ex1/Form1.vb
@@ -0,0 +1,319 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Public b As Bitmap
+    Public g As Graphics
+    Public r As New Random
+    Public PenTrajectory As New Pen(Color.OrangeRed, 2)
+    Public b2 As Bitmap
+    Public g2 As Graphics
+    Public b3 As Bitmap
+    Public g3 As Graphics
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Me.b = New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Me.g = Graphics.FromImage(b)
+        Me.g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g.Clear(Color.White)
+        Me.b2 = New Bitmap(Me.PictureBox2.Width, Me.PictureBox2.Height)
+        Me.g2 = Graphics.FromImage(b2)
+        Me.g2.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g2.Clear(Color.White)
+
+        Dim TrialsCount As Integer = TrackBar1.Value
+        Dim NumerOfTrajectories As Integer = TrackBar2.Value
+        Dim SuccessProbability As Double = TrackBar3.Value / TrialsCount
+
+        Dim minX As Double = 0
+        Dim maxX As Double = TrialsCount
+        Dim minY As Double = 0
+        Dim maxY As Double = TrialsCount
+
+        Dim interarrival = 0
+
+        Dim interarrivalDistribution As New Dictionary(Of Integer, Integer)
+
+        Me.b3 = New Bitmap(Me.PictureBox3.Width, Me.PictureBox3.Height)
+        Me.g3 = Graphics.FromImage(b3)
+        Me.g3.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g3.Clear(Color.White)
+
+        g3.DrawRectangle(New Pen(Color.Black, 1), 0, 0, PictureBox3.Width - 1, PictureBox3.Height - 1)
+
+        Dim VirtualWindow As New Rectangle(20, 20, Me.b.Width - 40, Me.b.Height - 40)
+
+        g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow)
+
+        Dim alt(Me.PictureBox1.Height) As Integer
+        For i As Integer = 0 To Me.PictureBox1.Height
+            alt(i) = 0
+        Next
+
+        For i As Integer = 1 To NumerOfTrajectories
+
+            Dim Punti As New List(Of Point)
+            Dim Y As Double = 0
+            For X As Integer = 1 To TrialsCount
+                Dim Uniform As Double = r.NextDouble
+                If Uniform < SuccessProbability Then
+                    Y = Y + 1
+                    If interarrival <> 0 Then
+                        If Not interarrivalDistribution.ContainsKey(interarrival) Then
+                            interarrivalDistribution.Add(interarrival, 1)
+                        Else
+                            interarrivalDistribution.Item(interarrival) = interarrivalDistribution.Item(interarrival) + 1
+                        End If
+                        interarrival = 0
+                    End If
+                Else
+                    interarrival = interarrival + 1
+                End If
+                Dim xDevice As Integer = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width)
+                Dim YDevice As Integer = FromYRealToYVirtual(Y, minY, maxY, VirtualWindow.Top, VirtualWindow.Height)
+                Punti.Add(New Point(xDevice, YDevice))
+            Next
+            g.DrawLines(PenTrajectory, Punti.ToArray)
+            alt(Punti.Last.Y) += 60
+        Next
+
+        Me.PictureBox1.Image = b
+        Dim pen As New Pen(Color.Black, 5)
+        Me.PictureBox2.Image = b2
+
+        For i As Integer = 0 To alt.Length - 1
+
+            g2.DrawLine(pen, 0, i, alt(i), i)
+
+        Next
+
+        drawVerticalChart(b3, g3, PictureBox3, interarrivalDistribution, TrialsCount)
+        PictureBox3.Image = b3
+
+    End Sub
+
+    Function drawVerticalChart(b As Bitmap, g As Graphics, p As PictureBox, distr As Dictionary(Of Integer, Integer), n As Integer)
+        Dim j As Integer
+        j = 0
+        Dim s As Integer
+        s = p.Width / distr.Count
+
+        g.DrawRectangle(New Pen(Color.Black), 0, 0, p.Width - 1, p.Height - 1)
+
+        For Each pair As KeyValuePair(Of Integer, Integer) In distr
+            Dim virtualX As Double = FromXRealToXVirtual(pair.Value, 0, n, 0, p.Height)
+            Dim r As Rectangle = New Rectangle(j + 1, p.Height - CType(virtualX, Integer) - 1, s, CType(virtualX, Integer))
+            g.DrawRectangle(New Pen(Color.Black), j + 1, p.Height - CType(virtualX, Integer) - 1, s, CType(virtualX, Integer))
+            g.FillRectangle(New SolidBrush(Color.Orange), r)
+            j = j + s
+
+        Next
+
+        p.Image = b
+
+    End Function
+
+    Function FromXRealToXVirtual(X As Double,
+                                 minX As Double, maxX As Double,
+                                 Left As Integer, W As Integer) As Integer
+
+        If (maxX - minX) = 0 Then
+            Return 0
+        End If
+
+        Return Left + W * (X - minX) / (maxX - minX)
+
+    End Function
+
+    Function FromYRealToYVirtual(Y As Double,
+                                minY As Double, maxY As Double,
+                                Top As Integer, H As Integer) As Integer
+
+        If (maxY - minY) = 0 Then
+            Return 0
+        End If
+
+        Return Top + H - H * (Y - minY) / (maxY - minY)
+
+    End Function
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Label1.Text = "Number of trials: " + TrackBar1.Value.ToString
+    End Sub
+
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        Label2.Text = "Number of trajectories: " + TrackBar2.Value.ToString
+    End Sub
+
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        Label3.Text = "Value of λ: " + (TrackBar3.Value).ToString
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Me.b = New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Me.g = Graphics.FromImage(b)
+        Me.g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g.Clear(Color.White)
+        Me.b2 = New Bitmap(Me.PictureBox2.Width, Me.PictureBox2.Height)
+        Me.g2 = Graphics.FromImage(b2)
+        Me.g2.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g2.Clear(Color.White)
+
+        Dim TrialsCount As Integer = TrackBar1.Value
+        Dim NumerOfTrajectories As Integer = TrackBar2.Value
+        Dim SuccessProbability As Double = TrackBar3.Value / TrialsCount
+
+        Dim minX As Double = 0
+        Dim maxX As Double = TrialsCount
+        Dim minY As Double = 0
+        Dim maxY As Double = TrialsCount
+
+        Dim interarrival = 0
+
+        Dim interarrivalDistribution As New Dictionary(Of Integer, Integer)
+
+        Me.b3 = New Bitmap(Me.PictureBox3.Width, Me.PictureBox3.Height)
+        Me.g3 = Graphics.FromImage(b3)
+        Me.g3.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g3.Clear(Color.White)
+
+
+        Dim VirtualWindow As New Rectangle(20, 20, Me.b.Width - 40, Me.b.Height - 40)
+
+        g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow)
+
+        Dim alt(Me.PictureBox1.Height) As Integer
+        For i As Integer = 0 To Me.PictureBox1.Height
+            alt(i) = 0
+        Next
+
+        For i As Integer = 1 To NumerOfTrajectories
+
+            Dim Punti As New List(Of Point)
+            Dim Y As Double = 0
+            For X As Integer = 1 To TrialsCount
+                Dim Uniform As Double = r.NextDouble
+                If Uniform <SuccessProbability Then
+                    Y = Y + 1
+                    If interarrival <> 0 Then
+                        If Not interarrivalDistribution.ContainsKey(interarrival) Then
+                            interarrivalDistribution.Add(interarrival, 1)
+                        Else
+                            interarrivalDistribution.Item(interarrival) = interarrivalDistribution.Item(interarrival) + 1
+                        End If
+                        interarrival = 0
+                    End If
+                Else
+                    interarrival = interarrival + 1
+                End If
+                Dim xDevice As Integer = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width)
+                Dim YDevice As Integer = FromYRealToYVirtual(Y * TrialsCount / (X + 1), minY, maxY, VirtualWindow.Top, VirtualWindow.Height)
+                Punti.Add(New Point(xDevice, YDevice))
+            Next
+            g.DrawLines(PenTrajectory, Punti.ToArray)
+            alt(Punti.Last.Y) += 60
+        Next
+
+        Me.PictureBox1.Image = b
+        Dim pen As New Pen(Color.Black, 4)
+        Me.PictureBox2.Image = b2
+        For i As Integer = 0 To alt.Length - 1
+
+            g2.DrawLine(pen, 0, i, alt(i), i)
+
+        Next
+
+        drawVerticalChart(b3, g3, PictureBox3, interarrivalDistribution, TrialsCount)
+        PictureBox3.Image = b3
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Me.b = New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Me.g = Graphics.FromImage(b)
+        Me.g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g.Clear(Color.White)
+        Me.b2 = New Bitmap(Me.PictureBox2.Width, Me.PictureBox2.Height)
+        Me.g2 = Graphics.FromImage(b2)
+        Me.g2.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g2.Clear(Color.White)
+
+        Dim TrialsCount As Integer = TrackBar1.Value
+        Dim NumerOfTrajectories As Integer = TrackBar2.Value
+        Dim SuccessProbability As Double = TrackBar3.Value / TrialsCount
+
+        Dim minX As Double = 0
+        Dim maxX As Double = TrialsCount
+        Dim minY As Double = 0
+        Dim maxY As Double = TrialsCount
+
+        Dim interarrival = 0
+
+        Dim interarrivalDistribution As New Dictionary(Of Integer, Integer)
+
+        Me.b3 = New Bitmap(Me.PictureBox3.Width, Me.PictureBox3.Height)
+        Me.g3 = Graphics.FromImage(b3)
+        Me.g3.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Me.g3.Clear(Color.White)
+
+        Dim VirtualWindow As New Rectangle(20, 20, Me.b.Width - 40, Me.b.Height - 40)
+
+        g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow)
+
+        Dim alt(Me.PictureBox1.Height) As Integer
+        For i As Integer = 0 To Me.PictureBox1.Height
+            alt(i) = 0
+        Next
+
+        For i As Integer = 1 To NumerOfTrajectories
+
+            Dim Punti As New List(Of Point)
+            Dim Y As Double = 0
+            For X As Integer = 1 To TrialsCount
+                Dim Uniform As Double = r.NextDouble
+                If Uniform < SuccessProbability Then
+                    Y = Y + 1
+                    If interarrival <> 0 Then
+                        If Not interarrivalDistribution.ContainsKey(interarrival) Then
+                            interarrivalDistribution.Add(interarrival, 1)
+                        Else
+                            interarrivalDistribution.Item(interarrival) = interarrivalDistribution.Item(interarrival) + 1
+                        End If
+                        interarrival = 0
+                    End If
+                Else
+                    interarrival = interarrival + 1
+                End If
+                Dim xDevice As Integer = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width)
+                Dim YDevice As Integer = FromYRealToYVirtual(Y * Math.Sqrt(TrialsCount) / Math.Sqrt(X + 1), minY, maxY, VirtualWindow.Top, VirtualWindow.Height)
+                Punti.Add(New Point(xDevice, YDevice))
+            Next
+            g.DrawLines(PenTrajectory, Punti.ToArray)
+            alt(Punti.Last.Y) += 60
+        Next
+
+        Me.PictureBox1.Image = b
+        Dim pen As New Pen(Color.Black, 4)
+        Me.PictureBox2.Image = b2
+        For i As Integer = 0 To alt.Length - 1
+
+            g2.DrawLine(pen, 0, i, alt(i), i)
+
+        Next
+
+        drawVerticalChart(b3, g3, PictureBox3, interarrivalDistribution, TrialsCount)
+        PictureBox3.Image = b3
+
+    End Sub
+
+End Class
